98ab1d613282f1e2f3fcfc0011d5afe5ed78398e
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-10-11 12:00:39 -0300

    Criando page contatos

--- src/pages/Clothes.jsx
+++ src/pages/Clothes.jsx
@@ -17,31 +17,31 @@ function Clothes() {
           <p>Até 6x de R$ 116,67 sem juros</p>
         </div>
         <div className="clothes-card">
-          <div className="bg-image-div" />
+          <div className="bg-image-div-2" />
           <p>Jaqueta M 666 - Tam Pp</p>
           <p>R$ 599,99</p>
           <p>Até 6x de R$ 99,99 sem juros</p>
         </div>
         <div className="clothes-card">
-          <div className="bg-image-div" />
+          <div className="bg-image-div-3" />
           <p>Jaqueta Caveira - Tam M</p>
           <p>R$ 599,99</p>
           <p>Até 6x de R$ 99,99 sem juros</p>
         </div>
         <div className="clothes-card">
-          <div className="bg-image-div" />
+          <div className="bg-image-div-4" />
           <p>Calça Vibes - Tam P</p>
           <p>R$ 699,99</p>
           <p>Até 6x de R$ 116,67 sem juros</p>
         </div>
         <div className="clothes-card">
-          <div className="bg-image-div" />
+          <div className="bg-image-div-5" />
           <p>Macacão Tribal - Tam M</p>
           <p>R$ 699,99</p>
           <p>Até 6x de R$ 116,67 sem juros</p>
         </div>
         <div className="clothes-card">
-          <div className="bg-image-div" />
+          <div className="bg-image-div-6" />
           <p>Colete Neon - Tam G</p>
           <p>R$ 699,99</p>
           <p>Até 6x de R$ 116,67 sem juros</p>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,12 @@
 import Header from "../components/Header";
 import Whats from "../components/Whats";
 import Footer from "../components/Footer";
+import insta from "../styles/imagens/svg/insta.svg";
+import facebook from "../styles/imagens/svg/facebook.svg";
+import tiktok from "../styles/imagens/svg/tiktok.svg";
+import email from "../styles/imagens/svg/email.svg";
+import whatsapp from "../styles/imagens/svg/whatsapp.svg";
+import "../styles/Contact.css";
 
 function Contact() {
   return (
@@ -8,8 +14,54 @@ function Contact() {
       <Header />
       <Whats />
       <Footer />
-      <div className="page-construction">
-        <h1>Página em construção!</h1>
+      <div className="contact-page">
+        <div>
+          <h2>Informações para contato:</h2>
+          <ul>
+            <li>Email: vendas@usemaarte.com.br</li>
+            <li>Email: sac@usemaarte.com.br</li>
+            <li>Telefone: 32 991370563</li>
+          </ul>
+        </div>
+        <div>
+          <div className="social-icons">
+            <a
+              href="https://www.tiktok.com/@usemaarte?_t=8gOd056Ctx0&_r=1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={tiktok} alt="TikTok" />
+            </a>
+            <a
+              href="https://www.facebook.com/people/Ma-Arte/61551698622143/?mibextid=ZbWKwL"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={facebook} alt="Facebook" />
+            </a>
+            <a
+              href="https://www.instagram.com/usemaarte/?igshid=OGQ5ZDc2ODk2ZA%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={insta} alt="Instagram" />
+            </a>
+            <a
+              href="mailto:sac@usemaarte.com.br"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={email} alt="Email" />
+            </a>
+            <a
+              href="https://api.whatsapp.com/send?phone=32991370563"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={whatsapp} alt="WhatsApp" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
